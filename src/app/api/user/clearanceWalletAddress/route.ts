a79d3a64de529377de4ba4429c9c85d1426d407f
seunghyonpark <creath.park@gmail.com>
2025-07-08 17:29:01 +0900

    fix: remove unused contract address variable for clarity in clearanceWalletAddress route

--- src/app/api/user/clearanceWalletAddress/route.ts
+++ src/app/api/user/clearanceWalletAddress/route.ts
@@ -79,8 +79,6 @@ export async function POST(request: NextRequest) {
 
   const contractAddressArbitrum = "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9"; // USDT on Arbitrum
 
-  const contractAddress = contractAddressPolygon;
-
 
   try {
 
