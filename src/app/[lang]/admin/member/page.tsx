0193e65f42e021764ea67ce6da9d79df435be18c
seunghyonpark <creath.park@gmail.com>
2025-07-11 11:13:00 +0900

    feat: add additional bank options to the selection dropdown

--- src/app/[lang]/admin/member/page.tsx
+++ src/app/[lang]/admin/member/page.tsx
@@ -2067,66 +2067,82 @@ export default function Index({ params }: any) {
                       onChange={(e) => setUserBankName(e.target.value)}
                       className="w-full p-2 border border-zinc-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                     >
-                      <option value="" selected={userBankName === ""}>
+                      <option value="">
                         은행선택
                       </option>
-                      <option value="카카오뱅크" selected={userBankName === "카카오뱅크"}>
+                      <option value="카카오뱅크">
                         카카오뱅크
                       </option>
-                      <option value="케이뱅크" selected={userBankName === "케이뱅크"}>
+                      <option value="케이뱅크">
                         케이뱅크
                       </option>
-                      <option value="토스뱅크" selected={userBankName === "토스뱅크"}>
+                      <option value="토스뱅크">
                         토스뱅크
                       </option>
-                      <option value="국민은행" selected={userBankName === "국민은행"}>
+                      <option value="국민은행">
                         국민은행
                       </option>
-                      <option value="우리은행" selected={userBankName === "우리은행"}>
+                      <option value="우리은행">
                         우리은행
                       </option>
-                      <option value="신한은행" selected={userBankName === "신한은행"}>
+                      <option value="신한은행">
                         신한은행
                       </option>
-                      <option value="농협" selected={userBankName === "농협"}>
+                      <option value="농협">
                         농협
                       </option>
-                      <option value="기업은행" selected={userBankName === "기업은행"}>
+                      <option value="신협">
+                        신협
+                      </option>
+                      <option value="새마을금고">
+                        새마을금고
+                      </option>
+                      <option value="우체국">
+                        우체국
+                      </option>
+                      <option value="산림조합">
+                        산림조합
+                      </option>
+                      <option value="SC제일은행">
+                        SC제일은행
+                      </option>
+                      <option value="기업은행">
                         기업은행
                       </option>
-                      <option value="하나은행" selected={userBankName === "하나은행"}>
+                      <option value="하나은행">
                         하나은행
                       </option>
-                      <option value="외환은행" selected={userBankName === "외환은행"}>
+                      <option value="외환은행">
                         외환은행
                       </option>
-                      <option value="부산은행" selected={userBankName === "부산은행"}>
+                      <option value="부산은행">
                         부산은행
                       </option>
-                      <option value="대구은행" selected={userBankName === "대구은행"}>
+                      <option value="대구은행">
                         대구은행
                       </option>
-                      <option value="전북은행" selected={userBankName === "전북은행"}>
+                      <option value="전북은행">
                         전북은행
                       </option>
-                      <option value="경북은행" selected={userBankName === "경북은행"}>
+                      <option value="경북은행">
                         경북은행
                       </option>
-                      <option value="광주은행" selected={userBankName === "광주은행"}>
+                      <option value="광주은행">
                         광주은행
                       </option>
-                      <option value="수협" selected={userBankName === "수협"}>
+                      <option value="수협">
                         수협
                       </option>
-                      <option value="씨티은행" selected={userBankName === "씨티은행"}>
+                      <option value="씨티은행">
                         씨티은행
                       </option>
-                      <option value="대신은행" selected={userBankName === "대신은행"}>
+                      <option value="대신은행">
                         대신은행
                       </option>
-                      <option value="동양종합금융" selected={userBankName === "동양종합금융"}>
+                      <option value="동양종합금융">
                         동양종합금융
                       </option>
+
                     </select>
 
                 
