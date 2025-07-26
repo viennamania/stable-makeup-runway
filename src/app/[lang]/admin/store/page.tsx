3bf9f215e6d50d864b989e440fb3c2ff8746c594
seunghyonpark <creath.park@gmail.com>
2025-07-26 23:38:19 +0900

    feat(page): add button to navigate to daily trade history statistics

--- src/app/[lang]/admin/store/page.tsx
+++ src/app/[lang]/admin/store/page.tsx
@@ -2044,6 +2044,18 @@ export default function Index({ params }: any) {
                     청산내역
                 </button>
 
+              <button
+                  onClick={() => router.push('/' + params.lang + '/admin/trade-history-daily')}
+                  className="flex w-32 bg-[#3167b4] text-[#f3f4f6] text-sm rounded-lg p-2 items-center justify-center
+                  hover:bg-[#3167b4]/80
+                  hover:cursor-pointer
+                  hover:scale-105
+                  transition-transform duration-200 ease-in-out
+                  ">
+                  통계(일별)
+              </button>
+
+
             </div>
 
 
